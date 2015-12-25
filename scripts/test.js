6f736ac70d679ed0634ae4ced0fb7393cfd8dc0c
ycjungSubhuman <jung3519@gmail.com>
2015-12-25 09:25:49 +0000

    Added canvas. Adding update function

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,33 @@
+function initCanvas(){//initiallize the canvas(for TESTING CANVAS DRAW)
+	//get canvas
+	var canvas = document.getElementById("visual");
+
+	if(canvas.getContext){//if successfully loaded
+		//init canvas context as "2d"
+		var ctx = canvas.getContext("2d");
+
+		//draw a red, opaque square
+		ctx.fillStyle = "rgb(200, 0, 0)";
+		ctx.fillRect(10, 10, 55, 50);
+
+		//draw a blue, transparent square
+		ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+		ctx.fillRect(30, 30, 55, 50);
+	}
+}
+
+function updateCanvas(frequencyData, bitcount){//in:Uint8Array, int size
+	//get canvas from HTML
+	var canvas = document.getElementById("visual");
+
+	if(canvas.getContext){//if successfully loaded
+
+		//clear canvas
+
+}
+
+
+
 window.onload = function(){
 	var ctx = new AudioContext();
 	var audio = document.getElementById("tangential");
@@ -8,14 +38,22 @@ window.onload = function(){
 	audioSrc.connect(analyser);
 	analyser.connect(ctx.destination);
 
-	var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+	//frequencyBinCount : number of values
+	frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
+	//loop to update frequency data
 	function renderFrame(){
 		requestAnimationFrame(renderFrame);
 		analyser.getByteFrequencyData(frequencyData);
+
+		/*implement rendering here*/
+		//updates canvas according to the frequencyData
+		updateCanvas(frequencyData, analyser.frequencyBinCount);
+
 	}
 
 	audio.play();
+	initCanvas();
 	renderFrame();
 };
 
